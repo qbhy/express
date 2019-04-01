93414d7ad2119b47e29718ab55a9cfec3b2b1f02
qbhy <572490755@qq.com>
2019-04-01 16:46:51 +0800

    fixed

--- src/Express.php
+++ src/Express.php
@@ -29,28 +29,47 @@ class Express
         if (is_null($type)) {
             throw new ExpressException("无用的快递单号: {$postId} 。");
         }
-
-        $url  = "https://www.kuaidi100.com/query?type={$type}&postid={$postId}&id=1&valicode=&temp=0.625568512055451&phone={$phone}";
+        $url  = "https://www.kuaidi100.com/query?type={$type}&postid={$postId}&temp=0.7003049469916303&phone={$phone}";
         $data = static::getHttp()->request('GET', $url)->getBody();
         return \GuzzleHttp\json_decode($data, true);
     }
 
-    /**
+    /**`
      * @return Client
      */
     public static function getHttp()
     {
         if (!(static::$http instanceof Client)) {
-            $chromeVersion = random_int(50, 73);
-            $osxVersion    = random_int(8, 13);
-            static::$http  = new Client([
+            $agents       = [
+                'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
+                'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
+                'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+                'Opera/9.80 (Windows NT 6.1; U; zh-cn) Presto/2.9.168 Version/11.50',
+                'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; Tablet PC 2.0; .NET4.0E)',
+                'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; InfoPath.3)',
+                'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; GTB7.0)',
+                'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+                'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)',
+                'Mozilla/5.0 (Windows; U; Windows NT 6.1; ) AppleWebKit/534.12 (KHTML, like Gecko) Maxthon/3.0 Safari/534.12',
+                'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E)',
+                'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E; SE 2.X MetaSr 1.0)',
+                'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.33 Safari/534.3 SE 2.X MetaSr 1.0',
+                'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E)',
+                'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.41 Safari/535.1 QQBrowser/6.9.11079.201',
+                'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E) QQBrowser/6.9.11079.201',
+                'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+            ];
+            static::$http = new Client([
                 RequestOptions::HEADERS => [
-                    'Referer'         => 'https://www.kuaidi100.com/',
-                    'User-Agent'      => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_{$osxVersion}_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{$chromeVersion}.0.3683.86 Safari/537.36",
-                    'Accept-Language' => 'zh,zh-CN;q=0.9,en;q=0.8',
-                    'Accept-Encoding' => 'gzip, deflate, br',
-                    'Connection'      => 'keep-alive',
-                ]
+                    'Referer'          => 'https://www.kuaidi100.com/',
+                    'User-Agent'       => $agents[random_int(0, count($agents) - 1)],
+                    'Accept-Language'  => 'zh,zh-CN;q=0.9,en;q=0.8',
+                    'Accept-Encoding'  => 'gzip, deflate, br',
+                    'Connection'       => 'keep-alive',
+                    'X-Requested-With' => 'XMLHttpRequest',
+                    'Host'             => 'www.kuaidi100.com',
+                    'Cookie'           => 'Hm_lvt_22ea01af58ba2be0fec7c11b25e88e6c=1554091249,1554099878; WWWID=WWW095E883265EF9D8F762C82F669FCF6EB; Hm_lpvt_22ea01af58ba2be0fec7c11b25e88e6c=1554107822'
+                ],
             ]);
         }
         return self::$http;
@@ -66,7 +85,7 @@ class Express
      */
     public static function queryType($postId)
     {
-        $data = \GuzzleHttp\json_decode(static::getHttp()->request('get', "http://www.kuaidi100.com/autonumber/autoComNum?text=$postId")->getBody(), true);
+        $data = \GuzzleHttp\json_decode(static::getHttp()->request('get', "http://www.kuaidi100.com/autonumber/autoComNum?text={$postId}")->getBody(), true);
         if (count($data['auto']) > 0) {
             return $data['auto'][0]['comCode'];
         } else {
